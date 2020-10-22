1e985a44735ae6d1fd864e36879727ed293521cd
cjmazzarri <cjmazzarri@gmail.com>
2020-10-22 18:36:41 -0500

    Finished Commerce.java

--- src/main/java/com/equilibrium/webapp/domain/model/Commerce.java
+++ src/main/java/com/equilibrium/webapp/domain/model/Commerce.java
@@ -1,35 +1,36 @@
 package com.equilibrium.webapp.domain.model;
 
-
-import lombok.Data;
-
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "commerces")
-public class Commerce extends AuditingEntityListener {
+public class Commerce extends AuditModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commerce_id;
 
     @NotNull
-    @NotBlank
     String first_name;
 
-    
+    @NotNull
     String last_name;
 
+    @NotNull
+    @Column(unique = true)
     String username;
 
+    @NotNull
     String password;
 
+    @Column(unique = true)
     String email_address;
 
+    @NotNull
     Date birth_date;
 }
