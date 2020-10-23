141177ece0ca2667027394f1e21b1471322b5806
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-23 17:50:23 -0500

    Created Repositories for Rate, MaintenanceFee, DeliveryFee.

---
+++ src/main/java/com/equilibrium/webapp/domain/repository/DeliveryFeeRepository.java
@@ -0,0 +1,6 @@
+package com.equilibrium.webapp.domain.repository;
+
+import com.equilibrium.webapp.domain.model.DeliveryFee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryFeeRepository extends JpaRepository<DeliveryFee, Long> { }
---
+++ src/main/java/com/equilibrium/webapp/domain/repository/RateRepository.java
@@ -0,0 +1,6 @@
+package com.equilibrium.webapp.domain.repository;
+
+import com.equilibrium.webapp.domain.model.Rate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RateRepository extends JpaRepository<Rate, Long> { }
