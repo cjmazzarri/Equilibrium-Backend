a17355ecc6d9ba9026d37f11a76409f3518c5cc4
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-22 19:25:41 -0500

    Create Rate.java

---
+++ src/main/java/com/equilibrium/webapp/domain/model/Rate.java
@@ -0,0 +1,27 @@
+package com.equilibrium.webapp.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "rates")
+@Getter
+@Setter
+public class Rate extends AuditModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Float value;
+
+    @NotNull
+    private String period;
+
+    @NotNull
+    private String type;
+}
