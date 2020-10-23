06a887fd59a0e7d383e617c56897398d6182ee5d
cjmazzarri <cjmazzarri@gmail.com>
2020-10-22 21:15:37 -0500

    Removed methods from Client Repository

--- src/main/java/com/equilibrium/webapp/domain/repository/ClientRepository.java
+++ src/main/java/com/equilibrium/webapp/domain/repository/ClientRepository.java
@@ -1,12 +1,6 @@
 package com.equilibrium.webapp.domain.repository;
 
 import com.equilibrium.webapp.domain.model.Client;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    Page<Client> FindByClientId(Long clientId, Pageable pageable);
-    Optional<Client> FindByIdAndClientId(Long id, Long clientId);
-}
+public interface ClientRepository extends JpaRepository<Client, Long> { }
