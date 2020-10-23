2592aa642571450b005e7cb231843a5e7ebfe1eb
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-23 18:10:13 -0500

    Created Commerce Resources.

---
+++ src/main/java/com/equilibrium/webapp/resource/CommerceResource.java
@@ -0,0 +1,18 @@
+package com.equilibrium.webapp.resource;
+
+import com.equilibrium.webapp.domain.model.AuditModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+public class CommerceResource extends AuditModel {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private Date birthDate;
+}
---
+++ src/main/java/com/equilibrium/webapp/resource/SaveCommerceResource.java
@@ -0,0 +1,45 @@
+package com.equilibrium.webapp.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+
+@Getter
+@Setter
+public class SaveCommerceResource {
+
+    @NotNull
+    @NotBlank
+    @Size(max = 30)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 30)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 50)
+    private String password;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String emailAddress;
+
+    @NotNull
+    @NotBlank
+    private Date birthDate;
+
+}
