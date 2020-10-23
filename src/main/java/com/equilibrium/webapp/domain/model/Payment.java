770e890023e1383840a41f3f947e4d7442985d95
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-22 20:24:42 -0500

    Create Payment.java

---
+++ src/main/java/com/equilibrium/webapp/domain/model/Payment.java
@@ -0,0 +1,38 @@
+package com.equilibrium.webapp.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payments")
+public class Payment extends AuditModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Client client;
+
+    @NotNull
+    @Lob
+    private String description;
+
+    @NotNull
+    private Float amount;
+
+    @NotNull
+    private Date date;
+}
