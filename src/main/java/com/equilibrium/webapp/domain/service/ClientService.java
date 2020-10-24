648c49633b74e75535a7b657c2422ce5a1dcea0a
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-23 22:53:07 -0500

    Created Client Service.

---
+++ src/main/java/com/equilibrium/webapp/domain/service/ClientService.java
@@ -0,0 +1,17 @@
+package com.equilibrium.webapp.domain.service;
+
+import com.equilibrium.webapp.domain.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public interface ClientService {
+    Page<Client> getAllClientsByCommerceId(Long commerceId, Pageable pageable);
+    Client getClientByIdAndCommerceId(Long commerceId, Long clientId);
+    Client createClient(Long commerceId, Client client);
+    Client updateClient(Long commerceId, Long clientId, Client clientDetails);
+    ResponseEntity<?> deleteClient(Long commerceId, Long clientId);
+    Client setClientRate(Long commerceId, Long clientId, Long rateId);
+    Client setClientMaintenanceFee(Long commerceId, Long clientId, Long maintenanceFeeId);
+    Client setClientDeliveryFee(Long commerceId, Long clientId, Long deliveryFeeId);
+}
---
+++ src/main/java/com/equilibrium/webapp/service/ClientServiceImpl.java
@@ -0,0 +1,110 @@
+package com.equilibrium.webapp.service;
+
+import com.equilibrium.webapp.domain.model.*;
+import com.equilibrium.webapp.domain.repository.*;
+import com.equilibrium.webapp.domain.service.ClientService;
+import com.equilibrium.webapp.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private CommerceRepository commerceRepository;
+
+    @Autowired
+    private RateRepository rateRepository;
+
+    @Autowired
+    private MaintenanceFeeRepository maintenanceFeeRepository;
+
+    @Autowired
+    private DeliveryFeeRepository deliveryFeeRepository;
+
+    @Override
+    public Page<Client> getAllClientsByCommerceId(Long commerceId, Pageable pageable) {
+        return clientRepository.findByCommerceId(commerceId, pageable); }
+
+    @Override
+    public Client getClientByIdAndCommerceId(Long commerceId, Long clientId) {
+        return clientRepository.findByIdAndCommerceId(commerceId, clientId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Client not found with Id " + clientId +
+                                " and CommerceId " + commerceId));
+    }
+
+    @Override
+    public Client createClient(Long commerceId, Client client) {
+        return commerceRepository.findById(commerceId).map(commerce -> {
+            client.setCommerce(commerce);
+            return clientRepository.save(client);
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "Commerce", "Id", commerceId));
+    }
+
+    @Override
+    public Client updateClient(Long commerceId, Long clientId, Client clientDetails) {
+        if(!commerceRepository.existsById(commerceId))
+            throw new ResourceNotFoundException("Commerce", "Id", commerceId);
+        return clientRepository.findById(clientId).map(client -> {
+            client.setFirstName(clientDetails.getFirstName());
+            client.setLastName(clientDetails.getLastName());
+            client.setCurrency(clientDetails.getCurrency());
+            client.setCreditAmount(clientDetails.getCreditAmount());
+            return clientRepository.save(client);
+        }).orElseThrow(() -> new ResourceNotFoundException("Client", "Id", clientId));
+    }
+
+    @Override
+    public ResponseEntity<?> deleteClient(Long commerceId, Long clientId) {
+        return clientRepository.findByIdAndCommerceId(commerceId, clientId).map(client -> {
+            clientRepository.delete(client);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "Client not found with Id " + clientId + " and CommerceId " + commerceId));
+    }
+
+    @Override
+    public Client setClientRate(Long commerceId, Long clientId, Long rateId) {
+        Rate rate = rateRepository.findById(rateId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Rate", "Id", rateId));
+        return clientRepository.findByIdAndCommerceId(commerceId, clientId).map(client -> {
+            client.setRate(rate);
+            return clientRepository.save(client);
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "Client", "Id", clientId));
+    }
+
+    @Override
+    public Client setClientMaintenanceFee(Long commerceId, Long clientId, Long maintenanceFeeId) {
+        MaintenanceFee maintenanceFee = maintenanceFeeRepository.findById(maintenanceFeeId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Maintenance Fee", "Id", maintenanceFeeId));
+        return clientRepository.findByIdAndCommerceId(commerceId, clientId).map(client -> {
+            client.setMaintenanceFee(maintenanceFee);
+            return clientRepository.save(client);
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "Client", "Id", clientId));
+    }
+
+    @Override
+    public Client setClientDeliveryFee(Long commerceId, Long clientId, Long deliveryFeeId) {
+        DeliveryFee deliveryFee = deliveryFeeRepository.findById(deliveryFeeId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Delivery Fee", "Id", deliveryFeeId));
+        return clientRepository.findByIdAndCommerceId(commerceId, clientId).map(client -> {
+            client.setDeliveryFee(deliveryFee);
+            return clientRepository.save(client);
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "Client", "Id", clientId));
+    }
+
+}
