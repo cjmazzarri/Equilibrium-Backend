1594a352da6d0b809445aa3320d4a01eca1c7cee
sadaakisz <hssm2000.sadaaki@gmail.com>
2020-10-23 22:54:53 -0500

    Fixed wrong import.

--- src/main/java/com/equilibrium/webapp/domain/model/Client.java
+++ src/main/java/com/equilibrium/webapp/domain/model/Client.java
@@ -1,13 +1,13 @@
 package com.equilibrium.webapp.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "clients")
